a1ea427efd2b06c31f208044202e868f1b796943
mario <abdelmounaimrihan@gmail.com>
2024-02-01 10:04:13 +0100

    DAY 3 DONE i used the ascii code to compare it if its repetead

---
+++ DAY 3 Isograms .js
@@ -0,0 +1,18 @@
+function isIsogram(str) {
+    str = str.toLowerCase(); // Convert to lowercase to handle case-insensitive comparison
+
+    // Iterate through each character in the string
+    for(let i=0 ; i<str.length; i++){
+        // Iterate through each character again
+        for(let j=i+1; j<str.length; j++){
+            // Check if the charCodeAt of the current character is equal to the charCodeAt of any other character
+            if(str.charCodeAt(i) == str.charCodeAt(j)){
+                return false; // If equal, it is not an isogram
+            }
+        }
+    }
+
+    // If we've made it through the loop without returning false, it's an isogram
+    return true;
+}
+
