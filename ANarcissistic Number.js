9f097df52756c599e994378609537f23f15d9651
SolidSnake <abdelmounaimrihan@gmail.com>
2024-02-08 10:06:07 +0100

    changed the console log to return so it can work on the lab

--- ANarcissistic Number.js
+++ ANarcissistic Number.js
@@ -1,20 +1,16 @@
 function narcissistic(value) {
    let seperate = String(value).split("")
-    let power = seperate.length
     let sum = 0
     for (let i = 0; i < seperate.length; i++) {
-      sum += Math.pow(seperate[i], power)
+      sum += Math.pow(seperate[i], seperate.length)
     }   
     if (sum === value) {
-      console.log(true)
+      return true
     } else {
-      console.log(false)
+      return false
     }
-
- 
-
   }
   
-  narcissistic(92727) ;
+
 
 
